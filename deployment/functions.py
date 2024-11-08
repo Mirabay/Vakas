4ab5f102c7483497a039294021c1c1816c9620f1
OshkarVTec <a01275287@tec.mx>
2024-11-08 11:36:31 -0600

    create functions to load and predict

---
+++ deployment/functions.py
@@ -0,0 +1,29 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+from VakaaCNN import SimpleCNN  # TODO: Fix the import
+
+
+def load_model(model_path):
+    model = SimpleCNN()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    return model
+
+
+def predict(model, image_path):
+    transform = transforms.Compose(
+        [
+            transforms.Resize(950, 450),
+            transforms.ToTensor(),
+        ]
+    )
+
+    image = Image.open(image_path).convert("RGB")
+    image = transform(image).unsqueeze(0)  # Add batch dimension
+
+    with torch.no_grad():
+        output = model(image)
+        _, predicted = torch.max(output, 1)
+
+    return predicted.item()
