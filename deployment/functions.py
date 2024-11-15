65c8a44aec518b93ed866178540ccc701560dae7
OshkarVTec <a01275287@tec.mx>
2024-11-15 09:21:12 -0600

    delete image perspective fix

--- deployment/functions.py
+++ deployment/functions.py
@@ -7,8 +7,6 @@ from PIL import Image
 import csv
 import os
 from datetime import datetime
-import cv2
-import numpy as np
 
 
 # Load the model. Returns the model
@@ -54,6 +52,7 @@ def crop_images_from_json(image_path, json_path):
         y = crop["y"]
         width = crop["width"]
         height = crop["height"]
+
         # Crop the image
         cropped_image = image.crop((x, y, x + width, y + height))
         images.append(cropped_image)
@@ -62,22 +61,7 @@ def crop_images_from_json(image_path, json_path):
 
 # Predict the class of an image from a full image and a JSON file with coordinates. Stores the prediction in a csv file
 def predict_and_save(model, image_path, coordinates_path, output_path):
-    # Read the image
-    img = cv2.imread(image_path)
-
-    # Perspective transformation
-    pts1 = np.float32([[0, 0], [0, 1920], [1080, 0], [1920, 1080]])
-    pts2 = np.float32([[0, 0], [0, 1850], [1080, 0], [1850, 1080]])
-    M = cv2.getPerspectiveTransform(pts1, pts2)
-    img = cv2.warpPerspective(img, M, (1680, 950))
-
-    # Save the transformed image to a temporary path
-    temp_image_path = "/tmp/transformed_image.jpg"
-    cv2.imwrite(temp_image_path, img)
-
-    # Use the transformed image for further processing
-    images = crop_images_from_json(temp_image_path, coordinates_path)
-
+    images = crop_images_from_json(image_path, coordinates_path)
     predictions = []
 
     for image in images:
@@ -85,6 +69,7 @@ def predict_and_save(model, image_path, coordinates_path, output_path):
         predictions.append(prediction)
 
     # Get the timestamp from the image path
+    # Extract the timestamp from the image filename
     filename = os.path.basename(image_path)
     timestamp_str = filename.split(".")[0]
 
@@ -107,6 +92,7 @@ def predict_and_save(model, image_path, coordinates_path, output_path):
             writer.writerow(column_names)
 
     # Append the predictions to the CSV file
+    row = [timestamp] + predictions
     with open(output_path, mode="a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([timestamp] + predictions)
+        writer.writerow(row)
